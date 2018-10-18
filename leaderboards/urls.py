82ce594788917567c00a4086bc65c7d9f13a5de8
Krakenos <1944193+Krakenos@users.noreply.github.com>
2018-10-18 19:33:38 +0200

    Made basic front to display leaderboards

--- leaderboards/urls.py
+++ leaderboards/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('ajax/leaderboards/<str:leaderboard_type>', views.get_leaderboard, name='get_leaderboard')
 ]
