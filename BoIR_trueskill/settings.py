27a2da41c51e7225af19fa4097f288abac6c4276
Krakenos <1944193+Krakenos@users.noreply.github.com>
2018-11-10 19:47:24 +0100

    some small changes in admin

--- BoIR_trueskill/settings.py
+++ BoIR_trueskill/settings.py
@@ -1,4 +1,5 @@
 import environ
+import os
 
 env = environ.Env(
     # set casting, default value
@@ -44,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'leaderboards.apps.LeaderboardsConfig'
+    'leaderboards'
 ]
 
 MIDDLEWARE = [
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'BoIR_trueskill.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- leaderboards/admin.py
+++ leaderboards/admin.py
@@ -2,8 +2,29 @@ from django.contrib import admin
 from .models import *
 from django.apps import apps
 
-# Register your models here.
 app = apps.get_app_config('leaderboards')
 
 for model_name, model in app.models.items():
     admin.site.register(model)
+
+admin.site.unregister(Tournament)
+
+
+class TournamentVodInLine(admin.TabularInline):
+    model = Vod
+    fields = ['url', 'description']
+
+
+class MatchInLine(admin.StackedInline):
+    model = Match
+    raw_id_fields = ('winner', 'loser', 'winner_team', 'loser_team')
+
+
+class TournamentAdmin(admin.ModelAdmin):
+    class Media:
+        js = ('/static/admin/js/hide_team_fields.js',)
+
+    inlines = [TournamentVodInLine, MatchInLine]
+
+
+admin.site.register(Tournament, TournamentAdmin)
